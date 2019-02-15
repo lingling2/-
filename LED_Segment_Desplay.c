cb8bcfbd02fcb7bc6bc0a93c8aff676661ad41bc
lingling2 <284391609@qq.com>
2019-02-15 18:16:22 +0800

    Add files via upload

---
+++ LED_Segment_Desplay.c
@@ -0,0 +1,97 @@
+#include<reg52.h>
+
+sbit ADDR0 = P1^0;
+sbit ADDR1 = P1^1;
+sbit ADDR2 = P1^2;
+sbit ADDR3 = P1^3;
+sbit ENLED = P1^4;
+
+unsigned char code LedChar[] = {
+	0xC0,0xF9,0xA4,0xB0,0x99,0x82,0xF8,
+	0x80,0x90,0x83,0xC6,0xA1,0x86,0x8E
+};
+
+unsigned char LedBuffer[] = {
+	0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+};
+
+void main(){
+	unsigned int cnt = 0;
+	unsigned long sec = 0;
+	unsigned char i = 0;
+	
+	ADDR3 = 1;
+	ENLED = 0;			//使能74HC138译码器
+	
+	TMOD = 0x01;		//定时器设置三步骤
+	TH0 = 0xFC;			//1.设置模式
+	TL0 = 0x67;			//2.设置初始值
+	TR0 = 1;			//3.启动定时器
+	
+	while(1){
+	if(TF == 1){
+		TF = 0;
+		TH0 = 0xFC;
+		TL0 = 0x67;
+		cnt++;
+		if(cnt>=1000){
+			cnt = 0;
+			sec++;
+			LedBuffer[0] = LedChar[sec%10];
+			LedBuffer[1] = LedChar[sec/10%10];
+			LedBuffer[2] = LedChar[sec/100%10];
+			LedBuffer[3] = LedChar[sec/1000%10];
+			LedBuffer[4] = LedChar[sec/10000%10];
+			LedBuffer[5] = LedChar[sec/100000%10];
+		}
+		//P0 = 0xFF;    //解决鬼影的方法：关闭段选
+		ENLED = 1;		//解决鬼影的方法：关闭位选
+		switch(i){
+			case 0:
+				ADDR0 = 0;
+				ADDR1 = 0;
+				ADDR2 = 0;
+				i++;
+				P0 = LedBuffer[0];
+				break;
+			case 1:
+				ADDR0 = 1;
+				ADDR1 = 0;
+				ADDR2 = 0;
+				i++;
+				P0 = LedBuffer[1];
+				break;
+			case 2:
+				ADDR0 = 0;
+				ADDR1 = 1;
+				ADDR2 = 0;
+				i++;
+				P0 = LedBuffer[2];
+				break;
+			case 3:
+				ADDR0 = 1;
+				ADDR1 = 1;
+				ADDR2 = 0;
+				i++;
+				P0 = LedBuffer[3];
+				break;
+			case 4:
+				ADDR0 = 0;
+				ADDR1 = 0;
+				ADDR2 = 1;
+				i++;
+				P0 = LedBuffer[4];
+				break;
+			case 5:
+				ADDR0 = 1;
+				ADDR1 = 0;
+				ADDR2 = 1;
+				i++;
+				P0 = LedBuffer[5];
+				break;
+			default:
+				break;
+		}
+		ENLED = 0;
+	}
+}
---
+++ heartbeat.c
@@ -0,0 +1,62 @@
+#include<reg52.h>
+
+sbit ADDR0 = P0^0;
+sbit ADDR1 = P0^1;
+sbit ADDR2 = P0^2;
+sbit ADDR3 = P0^3;
+sbit ENLED = P0^4;
+
+unsigned char code image[][]={
+	{0xFF,0x99,0x00,0x00,0x00,0x81,0xC3,0xE7},
+	{0xFF,0xFF,0xDB,0x81,0x81,0xC3,0xE7,0xFF},
+	{0xFF,0xFF,0xFF,0xDB,0xC3,0xE7,0xFF,0xFF},
+	{0xFF,0xFF,0xFF,0xFF,0xE7,0xE7,0xFF,0xFF},
+	{0xFF,0xFF,0xFF,0xFF,0xE7,0xE7,0xFF,0xFF},
+	{0xFF,0xFF,0xFF,0xFF,0xE7,0xE7,0xFF,0xFF},
+	{0xFF,0xFF,0xFF,0xDB,0xC3,0xE7,0xFF,0xFF},
+	{0xFF,0xFF,0xDB,0x81,0x81,0xC3,0xE7,0xFF},
+}
+void main(){
+	EA = 1;
+	ENLED = 0;
+	ADDR3 = 0;
+	
+	TMOD = 0x01;
+	TH0 = 0xFC;
+	TL0 = 0x67;
+	
+	ET0 = 1;
+	TR0 = 1;
+	while(1);
+}
+
+//定时器中断服务函数
+void InterruptTimer0() interrupt 1{
+	static unsigned char i = 0;
+	static unsigned char index = 0;
+	static unsigned char tmr = 0;
+	
+	TH0 = 0xFC;
+	TL0 = 0x67;
+	
+	P0 = 0xFF;
+	switch(i){
+		case 0:ADDR0=0;ADDR1=0;ADDR2=0;i++;P0=image[index][0];break;
+		case 1:ADDR0=1;ADDR1=0;ADDR2=0;i++;P0=image[index][1];break;
+		case 2:ADDR0=0;ADDR1=1;ADDR2=0;i++;P0=image[index][2];break;
+		case 3:ADDR0=1;ADDR1=1;ADDR2=0;i++;P0=image[index][3];break;
+		case 4:ADDR0=0;ADDR1=0;ADDR2=1;i++;P0=image[index][4];break;
+		case 5:ADDR0=1;ADDR1=0;ADDR2=1;i++;P0=image[index][5];break;
+		case 6:ADDR0=0;ADDR1=1;ADDR2=1;i++;P0=image[index][6];break;
+		case 7:ADDR0=1;ADDR1=1;ADDR2=1;i=0;P0=image[index][7];break;
+		default:break;
+	}
+	tmr++;
+	if(tmr>=200){
+		tmr = 0;
+		index++;
+		if(index>=8){
+			index = 0;
+		}
+	}
+}
